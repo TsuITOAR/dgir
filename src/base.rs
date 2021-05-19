edb197315251358c0784f3f587b067a40ed32345
TsuITOAR <itoar@outlook.com>
2021-05-19 21:07:33 +0800

    s

--- src/base.rs
+++ src/base.rs
@@ -1,41 +1,50 @@
-trait Attachable:Sized{
-    fn attach<E:Extendable>(self,e:&mut E)->&mut E{
+use ndarray;
+
+trait Attachable: Sized {
+    fn attach<E: Extendable>(self, e: &mut E) -> &mut E {
         e.extend(self)
     }
 }
 
-trait Extendable:Sized{
-    fn extend<A:Attachable>(&mut self,a:A)->&mut Self{
+trait Extendable: Sized {
+    fn extend<A: Attachable>(&mut self, a: A) -> &mut Self {
         unimplemented!();
     }
-    fn adapt<A:Adapter>(&mut self,adapter:A)->&mut Self{
+    fn adapt<A: Adapter>(&mut self, adapter: A) -> &mut Self {
         todo!()
     }
-    fn connect<T:Extendable,U>(&mut self,target:T)->U{
+    fn connect<T: Extendable, U>(&mut self, target: T) -> U {
         unimplemented!();
     }
 }
 
-trait Adapter:Sized{
-    fn reverse(self)->Self{
+trait Adapter: Sized {
+    fn reverse(self) -> Self {
         unimplemented!();
     }
 }
 
-impl<T:Adapter> Attachable for T{}
+impl<T: Adapter> Attachable for T {}
 
 /* trait Integrated:Sized{
     fn place()
 } */
 
-struct VectorInfo<P,A>{
-    position:(P,P),
-    direction:A,
-    layer:i16,
-    datatype:i16
+struct Arrow<P, A> {
+    position: (P, P),
+    direction: A,
 }
 
-struct Port<P=f64,A=f64,const N:usize=0>{
-    vector_info:VectorInfo<P,A>,
-    port:[;N]
+struct LayerData {
+    layer: i16,
+    datatype: i16,
+}
+
+struct Port<P = f64, A = f64, const N: usize=1> {
+    vector_info: Arrow<P, A>,
+    port: [(P, LayerData); N],
+}
+
+struct Polygon<T=f64>{
+    xy:ndarray::Array2<T>
 }
