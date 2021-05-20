62c07a2e1328dd424bfed4afde9c73f6fd91ff89
TsuITOAR <itoar@outlook.com>
2021-05-21 00:15:08 +0800

    impl a circle

--- src/base.rs
+++ src/base.rs
@@ -1,8 +1,8 @@
-use std::{convert::TryInto, usize};
+#![allow(unused_variables)]
+#![allow(dead_code)]
+use std::{convert::TryInto, iter::successors};
 
-use gds21::GdsBoundary;
-use ndarray;
-
+const MAX_POINTS_NUM: usize = 8191;
 trait Attachable: Sized {
     fn attach<E: Extendable>(self, e: &mut E) -> &mut E {
         e.extend(self)
@@ -33,76 +33,109 @@ impl<T: Adapter> Attachable for T {}
     fn place()
 } */
 
-struct Arrow<P, A> {
+pub struct Arrow<P, A> {
     position: (P, P),
     direction: A,
 }
 
-struct LayerData {
+pub struct LayerData {
     layer: i16,
     datatype: i16,
 }
+impl LayerData {
+    pub fn new(layer: i16, datatype: i16) -> Self {
+        Self { layer, datatype }
+    }
+}
 
-struct Port<P, A, const N: usize> {
+pub struct Port<P, A, const N: usize> {
     vector_info: Arrow<P, A>,
     port: [(P, LayerData); N],
 }
 
-struct Polygon<T = f64> {
-    xy: ndarray::Array2<T>,
+pub struct Polygon<T> {
+    xy: Vec<[T; 2]>,
     layer_data: LayerData,
 }
 
-impl<T> TryInto<gds21::GdsBoundary> for Polygon<T>
-where
-    T: Into<i32> + Copy,
-{
+impl TryInto<gds21::GdsBoundary> for Polygon<f64> {
     type Error = Box<dyn std::error::Error>;
     fn try_into(self) -> Result<gds21::GdsBoundary, Self::Error> {
-        let shape = self.xy.shape();
-        if shape[0] != 2 {
-            return Err("Incompatible xy dimension".into());
+        if self.xy.len() > MAX_POINTS_NUM {
+            return Err("too many points in a polygon".into());
         }
-        let len = shape[1] * 2;
         Ok(gds21::GdsBoundary {
             layer: self.layer_data.layer,
             datatype: self.layer_data.datatype,
             xy: self
                 .xy
-                .into_shape((1, len * 2))?
                 .into_iter()
-                .map(|x: &T| -> i32 { Into::<i32>::into(*x) })
+                .flatten()
+                .map(|x| num::ToPrimitive::to_i32(&x).unwrap())
                 .collect(),
             ..Default::default()
         })
     }
 }
 
-trait Curve {
-    fn line<T: Into<i32>>(&self) -> ndarray::Array2<T>;
+pub trait Curve: Sized {
+    fn points_iter(self) -> Box<dyn Iterator<Item = [f64; 2]>>;
+    fn connect_line<T: Curve>(self, other: T) -> Box<dyn Iterator<Item = [f64; 2]>> {
+        Box::new(self.points_iter().chain(other.points_iter()))
+    }
 }
 
-trait ClosedCurve: Curve {}
+pub trait ClosedCurve: Curve {}
 
-trait HasWidth {
-    fn width<T: Into<i32>>(&self) -> T;
+pub trait HasWidth {
+    fn width(&self) -> f64;
 }
 
-impl<T, U> From<(T, LayerData)> for Polygon<U>
+impl<T> From<(T, LayerData)> for Polygon<f64>
 where
-    T: ClosedCurve + HasWidth,
-    U: Into<i32>,
+    T: ClosedCurve,
 {
     fn from(s: (T, LayerData)) -> Self {
         let (line, layer_data) = s;
-        let xy = line.line::<U>();
+        let xy = line.points_iter().collect();
         Self { layer_data, xy }
     }
 }
 
-struct Circle<T>{
-    xy:ndarray::Array2<T>
+pub enum Resolution<T> {
+    MinDistance(T),
+    MinNumber(usize),
 }
-impl<T> Circle<T>{
-    fn new<U:Into<T>>(center:(T,T),radius:T)
+
+//TO-DO:make xy an enum composed of coordinates and iterator that return a coordinate
+pub struct Circle<T> {
+    xy: Vec<[T; 2]>,
 }
+impl Circle<f64> {
+    pub fn new(center: (f64, f64), radius: f64, resolution: Resolution<f64>) -> Self {
+        let two_pi = 2. * std::f64::consts::PI;
+        let points_num = match resolution {
+            Resolution::MinDistance(d) => {
+                num::ToPrimitive::to_usize(&(radius / d * two_pi)).unwrap()
+            }
+            Resolution::MinNumber(n) => n,
+        };
+
+        let ang_step: f64 = two_pi / num::ToPrimitive::to_f64(&(points_num - 1)).unwrap();
+        let ang_iter = successors(Some(0.), |x| Some(x + ang_step))
+            .take(points_num - 1)
+            .chain(std::iter::once(0.));
+
+        let point_list = ang_iter
+            .map(|arg| [center.0 + radius * arg.cos(), center.1 + radius * arg.sin()])
+            .collect();
+        Self { xy: point_list }
+    }
+}
+
+impl Curve for Circle<f64> {
+    fn points_iter(self) -> Box<dyn Iterator<Item = [f64; 2]>> {
+        Box::new(self.xy.into_iter())
+    }
+}
+impl ClosedCurve for Circle<f64> {}
--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,16 @@
-#![feature(const_generics_defaults)]
-struct Foo<const f: usize>;
+mod base;
+use std::convert::TryInto;
 
-fn main() {
-    println!("Hello, world!");
+use base::{Circle, LayerData, Polygon, Resolution::*};
+fn main() -> Result<(), Box<dyn std::error::Error>> {
+    let c = Circle::new((0., 0.), 5000., MinNumber(8191));
+    let p: Polygon<f64> = (c, LayerData::new(1, 1)).into();
+    use gds21::*;
+    let mut lib = GdsLibrary::new("mylib");
+    let mut newcell = GdsStruct::new("mycell");
+    let b = TryInto::<gds21::GdsBoundary>::try_into(p).unwrap();
+    newcell.elems.push(GdsElement::GdsBoundary(b));
+    lib.structs.push(newcell);
+    lib.save("example.gds")?;
+    Ok(())
 }
