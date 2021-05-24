48ce6c49ad440c69e8d89bb94feb42f3fa4bb46e
TsuITOAR <itoar@outlook.com>
2021-05-24 15:24:34 +0800

    update

--- src/draw/mod.rs
+++ src/draw/mod.rs
@@ -1,13 +1,21 @@
 mod elements;
 
-enum Drawer<In, Out> {
-    CurveEquation(Box<dyn Fn(Out) -> Out>),
-    ParametricEquation((Box<dyn Fn(In) -> Out>, Box<dyn Fn(In) -> Out>)),
-    DifferentialEquation(Box<dyn FnMut(Out) -> Out>),
-    DifferentialParametricEquation((Box<dyn FnMut(In) -> Out>, Box<dyn FnMut(In) -> Out>)),
+pub struct Drawer<In, Out> {
+    list: Box<dyn Iterator<Item = In>>,
+    x: Box<dyn FnMut(In) -> Out>,
+    y: Box<dyn FnMut(In) -> Out>,
 }
-enum Drawing<'a, T: 'a, S> {
-    Iter(Box<dyn Iterator<Item = T> + 'a>),
+
+impl<'a, In: 'a, Out: 'a> Drawer<In, Out> {
+    fn draw(self) -> Drawing<'a, Out> {
+        Drawing::Iter(Box::new(self.list.map(|p| [(self.x)(p), (self.y)(p)])))
+    }
+    fn decorate(&mut self, decorator: Box<dyn FnMut(In) -> In>) -> &mut Self {
+        self.list = self.list.map(|p| decorator(p));
+        self
+    }
+}
+enum Drawing<'a, T> {
+    Iter(Box<dyn Iterator<Item = [T; 2]> + 'a>),
     Points(Vec<[T; 2]>),
-    Drawer(Drawer<S, T>),
 }
