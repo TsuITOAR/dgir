63e97365e2b41cb39a9dc5c9252d2af75c095ec1
TsuITOAR <itoar@outlook.com>
2021-11-08 00:25:08 +0800

    bench

--- src/main.rs
+++ src/main.rs
@@ -10,19 +10,21 @@ use dgir::{
 
 fn main() -> Result<(), Box<dyn std::error::Error>> {
     let start = Instant::now();
-    let mut lib = DgirLibrary::new("test");
-    let cir = CircularArc::new(
-        100. * MICROMETER,
-        (zero(), zero()),
-        (Angle::from_deg(0.), Angle::from_deg(360.)),
-        Resolution::MinNumber(5000),
-    );
-    lib.push(
-        cir.into_curve()
-            .to_path(LayerData::new(1, 1))
-            .to_cell("cell_name"),
-    );
-    lib.save("test.gds").unwrap();
+    for _ in 1..100 {
+        let mut lib = DgirLibrary::new("test");
+        let cir = CircularArc::new(
+            100. * MICROMETER,
+            (zero(), zero()),
+            (Angle::from_deg(0.), Angle::from_deg(360.)),
+            Resolution::MinNumber(5000),
+        );
+        lib.push(
+            cir.into_curve()
+                .to_path(LayerData::new(1, 1))
+                .to_cell("cell_name"),
+        );
+        lib.save("test.gds").unwrap();
+    }
     println!("time costed:{}ms", start.elapsed().as_millis());
     Ok(())
 }
